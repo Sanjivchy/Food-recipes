c793c947a35e4c5d1bec6be6e5ca6b03e97c7df0
sanjivchy <sanjivchaudhary416@gmail.com>
2021-11-03 17:34:23 +0545

    final

--- src/App.js
+++ src/App.js
@@ -8,19 +8,20 @@ const APP_KEY="aec8513bcb76a02cdde88d2418d7e90a";
 
 const [recipe ,setRecipe] =useState([]);
 const [search, setSearch] =useState('');
-const [query, setQuery] = useState('chicken');
+const [query, setQuery] = useState('chowmein');
  
 useEffect(() => {
+  const getRecipe = async () => {
+    const exmpleReq=`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const respose = await fetch(exmpleReq)
+    const data =await respose.json();
+    setRecipe(data.hits);
+    console.log(data)
+  };
  getRecipe(); 
 }, [query]);
 
-const getRecipe = async () => {
-  const exmpleReq=`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  const respose = await fetch(exmpleReq)
-  const data =await respose.json();
-  setRecipe(data.hits);
-  console.log(data.hits)
-};
+
 
 const updateSearch = e =>{
   setSearch(e.target.value)
@@ -32,21 +33,27 @@ const getSearch = e => {
   setSearch('');
 }
   return (
-   <div>
-     <form onSubmit={getSearch}>
-       <input type="text" value={search} onChange={updateSearch}/>
-       <button type="submit" >Search</button>
+   <div className="bg-gray-600">
+     <div className="px-4 py-6 mb-10 bg-gray-700 w-fulls">
+     <form onSubmit={getSearch} className="" >
+       <input className="h-12 p-4 text-gray-600 bg-red-200 outline-none rounded-xl" type="text" placeholder="search here" value={search} required onChange={updateSearch}/>
+       <button type="submit" className="py-4 mx-4 mt-4 text-white bg-blue-500 md:m-0 hover:bg-blue-700 rounded-xl px-14" >Search</button>
      </form>
-
-     {recipe.map(recipe => (
-     <Recipes
-     key={recipe.recipe.label}
-     title={recipe.recipe.label} 
-     calories={recipe.recipe.calories}
-     image={recipe.recipe.image}
+     </div>
+  
+     <div className="grid grid-cols-1 gap-10 shadow-2xl lg:grid-cols-2">
+       {recipe.map(recipe => (
+      <Recipes
+      key={recipe.recipe.label}
+      title={recipe.recipe.label} 
+      calories={recipe.recipe.calories}
+      image={recipe.recipe.image}
+      ingredients={recipe.recipe.ingredients}
+      url={recipe.recipe.url}
       />
      ))}
-
+     </div>
+    
    </div>
   );
 }
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
-function Recipes({title,calories,image}) {
+function Recipes({title,calories,image , ingredients , url}) {
     return (
-        <div>
-            <h1>{title}</h1>
-            <p>{calories}</p>
-            <img src={image} alt="images"/>
+        <div className="mx-4 bg-white shadow-2xl focus:bg-gray-300 hover:bg-gray-300 rounded-2xl">
+            <h1 className="m-4 text-4xl font-bold text-center text-gray-700 border-b-2 border-gray-400">{title}</h1>
+            <div className="flex justify-center mx-4">
+                <img className="w-full h-auto max-w-lg rounded-2xl max-h-80" src={image} alt="images"/>
+            </div>
+
+            <div className="px-4 py-6 mx-6 my-8 bg-gray-100 shadow-md rounded-xl">
+                <ol className="h-64 overflow-auto text-left text-gray-600">
+                    {ingredients.map(ingredient => (
+                        <li className="py-2 text-xl">{ingredient.text}</li>
+                    ))}
+                </ol>
+            </div>
+            <div classname="">
+                <p className="mb-2 text-center text-gray-500 ">The Calories you will burn :{calories}</p>
+                <a href={url} className="block px-8 py-4 m-4 text-center text-white bg-blue-500 rounded-2xl">Read more</a>
+            </div>
+          
         </div>
     )
 }
